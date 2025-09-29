4a117d2c4dbd44b903acec31331403765eb0e37f
ashjang00 <ashliejireh@gmail.com>
2025-09-29 12:46:54 +0800

    Update and rename .gitignore to main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let courses = [
+  { code: "CS101", title: "Intro to Computer Science", credits: 3, department: "Computer Science" },
+  { code: "MATH201", title: "Linear Algebra", credits: 4, department: "Mathematics" },
+  { code: "PHYS150", title: "General Physics I", credits: 3, department: "Physics" },
+  { code: "ENG101", title: "English Composition", credits: 2, department: "English" },
+  { code: "CS202", title: "Data Structures", credits: 4, department: "Computer Science" }
+];
