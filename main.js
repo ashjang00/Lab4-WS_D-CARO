ae36c13d4bdeb0a9a0b4d0313ffe7cec3e51ff38
ashjang00 <ashliejireh@gmail.com>
2025-09-29 12:51:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ let courses = [
   { code: "ENG101", title: "English Composition", credits: 2, department: "English" },
   { code: "CS202", title: "Data Structures", credits: 4, department: "Computer Science" }
 ];
+function renderCatalog(list = courses) {
+  const catalog = document.getElementById("catalog");
+  catalog.innerHTML = "";
+  list.forEach(c => {
+    catalog.innerHTML += `
+      <div class="course">
+        <h3>${c.code}: ${c.title}</h3>
+        <p><b>Credits:</b> ${c.credits}</p>
+        <p><b>Department:</b> ${c.department}</p>
+      </div>
+    `;
+  });
+}
